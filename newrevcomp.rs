69a2efe03b95fc67a68f9fdc43a7bd2b571210ec
Tracy Ma <1991md@gmail.com>
2014-10-07 15:23:05 +0800

    use task

--- newrevcomp.rs
+++ newrevcomp.rs
@@ -4,32 +4,48 @@ use std::vec::Vec;
 static key: &'static str = "AaCcGgTtUuMmRrWwSsYyKkVvHhDdBbNn";
 static cmp: &'static str = "TTGGCCAAAAKKYYWWSSRRMMBBDDHHVVNN";
 
-fn revcomp(buffer: &mut Vec<u8>,split: &Vec<int>) {
-    let index = 0;
-    // TODO
-    // itor over split, malloc mem for splits
-    for i in range(begin, end) {
-        index = key.find();
-        buffer[i] = cmp[index];
+fn revcomp(buffer: &mut Vec<u8>, begin: int, end: int) {
+    for ii in range(begin, end) {
+        let result = key.find();
+        match result {
+            Some(index) => {
+                buffer[ii] = cmp[index];
+            },
+            None => ()
+        }
     }
 }
 
-fn split(buffer: Vec<u8>) -> &Vec<int> {
-    let mut vec = Vec::new();
+fn split(buffer: &Vec<u8>) {
+    let mut split = Vec::new();
     let index = 0;
+    let endline = false;
     for x in buffer.iter() {
         if (x == '<') {
-            vec.push(index)
+            split.push(index);
+            endline = true;
+        }
+        if (endline) {
+            split.push(index);
+            endline = false;
         }
         index++;
     }
-    return &vec;
+    spawn(proc() {
+        revcomp(buffer, split[1] + 1, split[2] - 1);
+    });
+    spawn(proc() {
+        revcomp(buffer, split[3] + 1, split[4] - 1);
+    });
+    spawn(proc() {
+        revcomp(buffer, split[5] + 1, split[4] - 1);
+    });
 }
 
 fn main() {
     let result = io::stdin().read_to_end();
     match result {
-        Ok(buffer) => println!("{}", buffer), // vec<u8>
+        Ok(buffer) => split(&buffer), // vec<u8>
         Err(_) => println!("error")
     }
 }
