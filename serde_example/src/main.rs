52fdbcbea44d773346a1ddc041181aaf5d672654
Tracy Ma <1991md@gmail.com>
2015-06-26 07:51:20 +0800

    directly deserialize to Vec<f64>

--- serde_example/src/main.rs
+++ serde_example/src/main.rs
@@ -4,19 +4,19 @@ use serde::json::{self, Value};
 // file io
 use std::io::prelude::*;
 use std::fs::File;
+//
+use std::collections::BTreeMap;
 
 fn main() {
 	let mut f = File::open("./orc.c3t").unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s);
     let data: Value = json::from_str(&s).unwrap();
-    let obj = data.as_object().unwrap();
-    let meshes = obj.get("meshes").unwrap();
+    let meshes = data.find("meshes").unwrap();
     let mesh_array = meshes.as_array().unwrap();
     let mesh = mesh_array[0].as_object().unwrap();
-    let vertices: Vec<json::Value> = mesh.get("vertices").unwrap().as_array().unwrap().to_vec();
-    let vertex_array: Vec<f64> = vertices.into_iter().map (|value| json::from_value (value) .unwrap()) .collect();
-    for pos in vertex_array {
+    let vertices: Vec<f64> = (json::from_value (mesh.get("vertices").unwrap()) ).unwrap();
+    for pos in vertices {
     	println!("{}", pos);
     }
  }
