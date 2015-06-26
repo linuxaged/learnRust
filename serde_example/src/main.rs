5ea3aa54cc90cba5b8c72994c2f4abd8fe026f05
Tracy Ma <madi@chukong-inc.com>
2015-06-26 11:22:28 +0800

    space

--- serde_example/src/main.rs
+++ serde_example/src/main.rs
@@ -6,7 +6,7 @@ use std::io::prelude::*;
 use std::fs::File;
 
 fn main() {
-	let mut f = File::open("./orc.c3t").unwrap();
+    let mut f = File::open("./orc.c3t").unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s);
     let data: Value = json::from_str(&s).unwrap();
@@ -17,6 +17,6 @@ fn main() {
     let vertices: Vec<json::Value> = mesh.get("vertices").unwrap().as_array().unwrap().to_vec();
     let vertex_array: Vec<f64> = vertices.into_iter().map (|value| json::from_value (value) .unwrap()) .collect();
     for pos in vertex_array {
-    	println!("{}", pos);
+        println!("{}", pos);
     }
  }
