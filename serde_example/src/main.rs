293a42865b05af0bf03c41d9a999087daababfd4
Tracy Ma <madi@chukong-inc.com>
2015-06-30 17:39:56 +0800

    cast to Vec<Vertex> ok

--- serde_example/src/main.rs
+++ serde_example/src/main.rs
@@ -1,19 +1,20 @@
-#![feature(custom_derive, plugin)]
+#![feature(custom_derive, plugin,step_by)]
 #![plugin(serde_macros)]
 extern crate serde;
 use serde::json::{self, Value};
 // file io
 use std::io::prelude::*;
 use std::fs::File;
-#[macro_use]
-extern crate itertools;
-use itertools::Itertools;
 
 fn main() {
-    let mut f = File::open("./test.json").unwrap();
+    let mut f = File::open("./orc.c3t").unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s);
     let data: Value = json::from_str(&s).unwrap();
+    let meshes = data.find("meshes").unwrap();
+    let mesh_array = meshes.as_array().unwrap();
+    let mesh = mesh_array[0].as_object().unwrap();
+
     #[derive(Copy, Clone, Serialize, Deserialize, Display, Debug)]
     struct Vertex {
         position:   [f64; 3],
@@ -25,59 +26,21 @@ fn main() {
     // let vs = Vertex{position:[1.0;3],normal:[1.0;3],texcood:[1.0;2],blendweight:[1.0;4],blendindex:[1.0;4]};
     // let serialized_vertices = json::to_string(&vs).unwrap();
     // println!("{:?}", serialized_vertices);
-    let vertices: Vec<f64> = (json::from_value (data.find("vertices").unwrap().clone()) ).unwrap();
+    let vertices: Vec<f64> = (json::from_value(mesh.get("vertices").unwrap().clone()) ).unwrap();
     
-    let vertex_array = vertices.batching(|mut it| {
-        match it.next() {
-            None => None,
-            Some(pos_x) => match it.next() {
-                None => None,
-                Some(pos_y) => match it.next() {
-                    None => None,
-                    Some(pos_z) => match it.next() {
-                        None => None,
-                        Some(nor_x) => match it.next() {
-                            None => None,
-                            Some(nor_y) => match it.next() {
-                                None => None,
-                                Some(nor_z) => match it.next() {
-                                    None => None,
-                                    Some(tex_x) => match it.next() {
-                                        None => None,
-                                        Some(tex_y) => match it.next() {
-                                            None => None,
-                                            Some(bw_x) => match it.next() {
-                                                None => None,
-                                                Some(bw_y) => match it.next() {
-                                                    None => None,
-                                                    Some(bw_z) => match it.next() {
-                                                        None => None,
-                                                        Some(bw_w) => match it.next() {
-                                                            None => None,
-                                                            Some(bi_x) => match it.next() {
-                                                                None => None,
-                                                                Some(bi_y) => match it.next() {
-                                                                    None => None,
-                                                                    Some(bi_z) => match it.next() {
-                                                                        None => None,
-                                                                        Some(bi_w) => Some(Vertex{position:[pos_x,pos_y,pos_z], normal:[nor_x,nor_y,nor_z], texcood:[tex_x, tex_y], blendweight:[bw_x,bw_y,bw_z,bw_w], blendindex:[bi_x,bi_y,bi_z,bi_w]}),
-                                                                        },
-                                                                    },
-                                                                },
-                                                            },
-                                                        },
-                                                    },
-                                                },
-                                            },
-                                        },
-                                    },
-                                },
-                            },
-                        },
-                   },
-               },
-           }
-       });
+    let mut vertex_array:Vec<Vertex> = Vec::<Vertex>::new();
+    
+    for i in (0..vertices.len()).step_by(16) {
+        let vertex = Vertex{
+            position:[vertices[i+0], vertices[i+1],vertices[i+2]],
+            normal:[vertices[i+3],vertices[i+4],vertices[i+5]],
+            texcood:[vertices[i+6],vertices[i+7]],
+            blendweight:[vertices[i+8], vertices[i+9],vertices[i+10],vertices[i+11]],
+            blendindex:[vertices[i+12], vertices[i+13],vertices[i+14],vertices[i+15]]
+        };
+        vertex_array.push(vertex);
+    }
+
     for pos in vertex_array {
     	println!("{:?}", pos);
     }
