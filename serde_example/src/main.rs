2d954d79bf7fcd93c87d7f02800e4956a4a3c5bc
Tracy Ma <madi@chukong-inc.com>
2015-07-09 18:44:11 +0800

    custom deserializer for struct

--- serde_example/src/main.rs
+++ serde_example/src/main.rs
@@ -15,15 +15,32 @@ fn main() {
     let mesh_array = meshes.as_array().unwrap();
     let mesh = mesh_array[0].as_object().unwrap();
 
-    #[derive(Copy, Clone, Serialize, Deserialize, Display, Debug)]
+    #[derive(Copy, Clone, Serialize, Display, Debug)]
     struct Vertex {
         position:   [f64; 3],
         normal:     [f64; 3],
-        texcood:    [f64; 2],
+        texcoord:    [f64; 2],
         blendweight:[f64; 4],
         blendindex: [f64; 4]
     }
-    // let vs = Vertex{position:[1.0;3],normal:[1.0;3],texcood:[1.0;2],blendweight:[1.0;4],blendindex:[1.0;4]};
+
+    impl serde::Deserialize for Vertex {
+        #[inline]
+        fn deserialize<D>(deserializer: &mut D) -> Result<Vertex, D::Error>
+            where D: serde::Deserializer {
+
+            let vertices: [f64; 16] = try!(serde::Deserialize::deserialize(deserializer));
+
+            Ok(Vertex {
+                position:[vertices[0], vertices[1],vertices[2]],
+                normal:[vertices[3], vertices[4], vertices[5]],
+                texcoord:[vertices[6], vertices[7]],
+                blendweight:[vertices[8], vertices[9], vertices[10], vertices[11]],
+                blendindex:[vertices[12], vertices[13],vertices[14], vertices[15]]
+            })
+        }
+    }
+    // let vs = Vertex{position:[1.0;3],normal:[1.0;3],texcoord:[1.0;2],blendweight:[1.0;4],blendindex:[1.0;4]};
     // let serialized_vertices = json::to_string(&vs).unwrap();
     // println!("{:?}", serialized_vertices);
     let vertices: Vec<f64> = (json::from_value(mesh.get("vertices").unwrap().clone()) ).unwrap();
@@ -34,15 +51,21 @@ fn main() {
         let vertex = Vertex{
             position:[vertices[i+0], vertices[i+1],vertices[i+2]],
             normal:[vertices[i+3],vertices[i+4],vertices[i+5]],
-            texcood:[vertices[i+6],vertices[i+7]],
+            texcoord:[vertices[i+6],vertices[i+7]],
             blendweight:[vertices[i+8], vertices[i+9],vertices[i+10],vertices[i+11]],
             blendindex:[vertices[i+12], vertices[i+13],vertices[i+14],vertices[i+15]]
         };
         vertex_array.push(vertex);
     }
 
-    for pos in vertex_array {
-    	println!("{:?}", pos);
-    }
+    let val: Value = json::from_str(r#"{"floats":[1.0,2.0,3.0,4.0,
+        5.0,6.0,7.0,8.0,
+        9.0,10.0,11.0,12.0,
+        13.0,14.0,15.0,16.0]}"#).unwrap();
+    let fs:Vertex = json::from_value(val.find("floats").unwrap().clone() ).unwrap() ;
+    println!("{:?}", fs);
+    // for pos in vertex_array {
+    // 	println!("{:?}", pos);
+    // }
 
  }
